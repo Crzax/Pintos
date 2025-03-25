8065c658cd7d50b80a5b9bf7f5db4558fa07f055
Crzax <czx@mit.edu>
2025-03-25 21:01:17 +0800

    [fix] Make filesystem be compilered right.

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -133,7 +133,7 @@ struct thread
    If true, use multi-level feedback queue scheduler.
    Controlled by kernel command-line option "-o mlfqs". */
 extern bool thread_mlfqs;
-int load_avg;
+extern int load_avg;
 
 void thread_init (void);
 void thread_start (void);
