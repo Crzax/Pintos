2a2cfea8bf8934049935929a6115790b199684ae
Crzax <czx@mit.edu>
2025-03-27 19:42:54 +0800

    [feat] Add Buffer Cache.

---
+++ src/filesys/cache.c
@@ -0,0 +1,148 @@
+#include "filesys/cache.h"
+#include "filesys/filesys.h"
+#include "threads/malloc.h"
+
+#include "threads/synch.h"
+#include "threads/thread.h"
+
+#include "devices/timer.h"
+
+#include <string.h>
+#include <bitmap.h>
+#include <stdint.h>
+#include <hash.h>
+
+#define FLUSH_INTERVAL (5 * TIMER_FREQ) 
+
+static void cache_flush_slot (size_t slot_idx);
+static void cache_flush (void);
+static void cache_flush_daemon(void* aux UNUSED);
+
+struct block_slot {
+  block_sector_t sector;              /**< Sector index in block device. */
+  uint8_t data[BLOCK_SECTOR_SIZE];
+  bool accessed;
+  bool dirty;
+  uint32_t user_count;
+};
+
+struct block_slot *buffer_cache;
+struct bitmap *free_slots;
+
+struct lock cache_lock;
+
+
+void cache_init (void) {
+  buffer_cache = malloc(sizeof(struct block_slot) * CACHE_CAPACITY);
+  int i;
+  for (i = 0; i < CACHE_CAPACITY; i++) {
+    buffer_cache[i].sector = -1;  /**< At the beginning there must be no cache hits. */
+    buffer_cache[i].accessed = false;
+    buffer_cache[i].dirty = false;
+    buffer_cache[i].user_count = 0;
+  }
+  
+  free_slots = bitmap_create(CACHE_CAPACITY);
+  lock_init(&cache_lock);
+
+  thread_create("cache-flush-daemon", PRI_DEFAULT, cache_flush_daemon, NULL);
+}
+
+static void cache_flush_daemon(void* aux UNUSED) {
+  while (true) {
+      timer_sleep(FLUSH_INTERVAL);
+      cache_flush();
+  }
+}
+
+void cache_destroy (void) {
+  cache_flush ();
+  free(buffer_cache);
+  bitmap_destroy(free_slots);
+}
+
+
+/* Helpers */
+
+static int cache_find (block_sector_t sector) {
+  int i = 0;
+  for (; i < CACHE_CAPACITY; i++)
+    if (buffer_cache[i].sector == sector)
+      return i;
+  return -1;
+}
+
+static void cache_flush_slot (size_t slot_idx) {
+  block_write (fs_device, buffer_cache[slot_idx].sector, buffer_cache[slot_idx].data);
+  buffer_cache[slot_idx].dirty = false;
+}
+
+static void cache_flush (void) {
+  lock_acquire(&cache_lock);
+  int i = 0;
+  for (; i < CACHE_CAPACITY; i++) {
+    if (buffer_cache[i].dirty)
+      cache_flush_slot (i);
+    if (buffer_cache[i].accessed)
+      buffer_cache[i].accessed = false;
+    if (buffer_cache[i].user_count == 0)
+      bitmap_reset (free_slots, i);
+  }
+  lock_release(&cache_lock);
+}
+
+static int cache_get_slot (void) {
+  size_t i = bitmap_scan_and_flip(free_slots, 0, 1, false);
+  if (i != BITMAP_ERROR) return i;
+  while (true) {
+    i = 0;
+    for (; i < CACHE_CAPACITY; i++) {
+      if (buffer_cache[i].accessed)
+        buffer_cache[i].accessed = false;
+      else if (buffer_cache[i].user_count == 0) { /**< Evict */
+        if (buffer_cache[i].dirty)  /**< Flush */
+          cache_flush_slot (i);
+        return i;
+      }
+    }
+  }
+}
+
+static int cache_load (block_sector_t sector) {
+  int slot_idx = cache_get_slot ();
+  buffer_cache[slot_idx].sector = sector;
+  block_read (fs_device, sector, buffer_cache[slot_idx].data);
+  return slot_idx;
+}
+
+void cache_read (block_sector_t sector, int sector_ofs, off_t buffer_ofs, size_t size, void *buffer_) {
+  uint8_t *buffer = buffer_;
+
+  lock_acquire(&cache_lock);
+  int slot_idx = cache_find (sector);
+  if (slot_idx == -1) /* Cache miss */
+    slot_idx = cache_load (sector);
+  buffer_cache[slot_idx].user_count++;
+  lock_release(&cache_lock);
+
+  memcpy (buffer + buffer_ofs, buffer_cache[slot_idx].data + sector_ofs, size);
+  buffer_cache[slot_idx].accessed = true;
+  buffer_cache[slot_idx].user_count--;
+}
+
+
+void cache_write (block_sector_t sector, int sector_ofs, off_t buffer_ofs, size_t size, const void *buffer_) {
+  const uint8_t *buffer = buffer_;
+
+  lock_acquire(&cache_lock);
+  int slot_idx = cache_find (sector);
+  if (slot_idx == -1) /* Cache miss */
+    slot_idx = cache_load (sector);
+  buffer_cache[slot_idx].user_count++;
+  lock_release(&cache_lock);
+
+  memcpy (buffer_cache[slot_idx].data + sector_ofs, buffer + buffer_ofs, size);
+  buffer_cache[slot_idx].accessed = true;
+  buffer_cache[slot_idx].dirty = true;
+  buffer_cache[slot_idx].user_count--;
+}
